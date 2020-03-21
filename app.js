7509d73bd607e6c731982039a5faf120b07f4b24
Amazonian32 <ynitsedekralc@gmail.com>
2020-03-20 21:12:30 -0600

    The app is done. Finally got the the functions in the right order and syntax to be able to add new topics and those new topics will show gifs. The gifs play and pause on click

--- app.js
+++ app.js
@@ -13,8 +13,12 @@ $(document).ready(function () {
         "Aesthetic",
         "Rock n Roll"];
 
-    let buttonPops = function () {
+// making the topics buttons
+    function renderButtons() {
+        $("#buttons").empty();
+
         for (let i = 0; i < topics.length; i++) {
+
             let topic = topics[i]
             var category = $("<button>")
             category.addClass("btn")
@@ -26,17 +30,30 @@ $(document).ready(function () {
         };
 
     }
-    buttonPops();
-
-
+    renderButtons();
+// adding new topics
+    $(".searchButton").on("click", function (event) {
+        event.preventDefault();
+        var search = $("#search").val().trim();
+        topics.push(search);
+        renderButtons();
+    });
+// function for the API
     let gifPopulate = function () {
+
+        let choice = $(this).attr("id");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5mXKXvoG27jxPSeOXNPOxkO9GSWsxtIl&q=" + choice;
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
+            $("#images").empty();
             console.log(response);
             let gifs = response.data
-            for (let i = 0; i < gifs.length; i++) {
+
+            // forloop for the topics
+            for (let i = 0; i < 10; i++) {
                 // gif coding
                 var leGif = $("<img>")
                 leGif.attr("alt", gifs[i].title)
@@ -53,41 +70,31 @@ $(document).ready(function () {
                 // putting it on the page
                 ratings.append(gifRate, leGif);
                 $("#images").append(ratings);
-            }
+                // console.log($(".gif")[i].dataset)
+            };
+            // playing and pausing gifs
+            $("img").on("click", function (event) {
+                event.preventDefault();
+                var gif = $(this)
+                var state = gif.attr("data-state")
+                var animate = gif.attr("data-animate")
+                var still = gif.attr("data-still")
+                if (state == "still") {
+                    gif.attr("src", animate)
+                    gif.attr("data-state", "animate")
+                } else {
+                    gif.attr("src", still)
+                    gif.attr("data-state", "still")
+                }
+
+
+            })
+
         });
+
     }
-    $(".topicButton").on("click", function (event) {
-        event.preventDefault();
-        $("#images").empty();
-        let choice = $(this).attr("id");
-        console.log(choice);
-        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5mXKXvoG27jxPSeOXNPOxkO9GSWsxtIl&q=" + choice + "&limit=10";
-        gifPopulate();
+    // makings the gifs show up on click
+    $(document).on("click", ".topicButton", gifPopulate)
 
-    });
-    $(".searchButton").on("click", function (event) {
-        event.preventDefault();
-        $("#images").empty();
-        let search = $("#search").val().trim();
-        console.log(search);
-        queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5mXKXvoG27jxPSeOXNPOxkO9GSWsxtIl&q=" + search + "&limit=10";
-        gifPopulate();
 
-    });
-    $(".gif").on("click", function (event) {
-        event.preventDefault();
-        console.log("clicked")
-        var gif = $(this)
-        var state = gif.attr("data-state")
-        var animate = gif.attr("data-animate")
-        var still = gif.attr("data-still")
-        if (state == "still") {
-          gif.attr("src", animate)
-          gif.attr("data-state", "animate")
-        } else {
-          gif.attr("src", still)
-          gif.attr("data-state", "still")
-        }
-      });
-
-})
+});
